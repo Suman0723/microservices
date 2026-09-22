5e42acd957a7207a1ed6371f4f5634d249516183
suman <sumannekko07@gmail.com>
2026-09-22 15:08:59 +0530

    Complete Day 9 resilience monitoring and bulkhead

--- OrderService/src/main/java/com/example/demo/service/UserServiceResilientClient.java
+++ OrderService/src/main/java/com/example/demo/service/UserServiceResilientClient.java
@@ -8,31 +8,30 @@ import com.example.demo.exception.DependencyUnavailableException;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class UserServiceResilientClient {
 
-    private final UserClient userClient;
+	private final UserClient userClient;
 
-    @Retry(
-            name = "userService",
-            fallbackMethod = "getUserFallback"
-    )
-    @CircuitBreaker(
-            name = "userService",
-            fallbackMethod = "getUserFallback"
-    )
-    public UserDto getUserById(Long userId) {
+	@Bulkhead(name = "userService", fallbackMethod = "getUserFallback")
 
-        return userClient.getUserById(userId);
-    }
+	@Retry(name = "userService", fallbackMethod = "getUserFallback")
+	@CircuitBreaker(name = "userService", fallbackMethod = "getUserFallback")
+	public UserDto getUserById(Long userId) {
+		log.info("Calling User Service for userId: {}", userId);
 
-    public UserDto getUserFallback(
-            Long userId,
-            Throwable ex) {
+		return userClient.getUserById(userId);
+	}
 
-        throw new DependencyUnavailableException(
-                "User Service is temporarily unavailable");
-    }
+	public UserDto getUserFallback(Long userId, Throwable ex) {
+
+		log.error("User Service fallback executed for userId: {}. Reason: {}", userId, ex.getMessage());
+
+		throw new DependencyUnavailableException("User Service is temporarily unavailable");
+	}
 }
